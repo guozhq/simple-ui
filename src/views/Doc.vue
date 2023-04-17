a7df7645e12408285ad26662fa61a7d3738c4b07
guozhq <shxguozhq@qq.com>
2023-04-17 18:09:07 +0800

    完成首页和文档页面简单样式

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -18,3 +18,23 @@
     </aside>
   </div>
 </template>
+<style lang="scss" scoped>
+.top_nav {
+  background: #ccccff;
+  display: flex;
+  max-width: 100%;
+  padding: 16px;
+  justify-content: space-between;
+
+  .logo {
+    max-width: 6em;
+  }
+
+  .menu {
+    display: flex;
+
+    li {
+      margin: 0 1em;
+    }
+  }
+}</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,3 +15,40 @@
     </p>
   </div>
 </template>
+<style lang="scss" scoped>
+.top_nav {
+  background: #ccccff;
+  display: flex;
+  max-width: 100%;
+  padding: 16px;
+  justify-content: space-between;
+
+  .logo {
+    max-width: 6em;
+  }
+
+  .menu {
+    display: flex;
+
+    li {
+      margin: 0 1em;
+    }
+  }
+}
+
+.banner {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 100px 0;
+  background: rgba(94, 171, 127, 1);
+
+  .actions {
+    padding: 8px 0;
+
+    a {
+      margin: 0 8px;
+    }
+  }
+}
+</style>
