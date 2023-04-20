bb87e04169c64133b356a9e0030a1d39b8682163
guozhq <shxguozhq@qq.com>
2023-04-20 18:46:53 +0800

    修改首页github链接

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       <h1>Simple UI</h1>
       <h2>一个简单的UI框架</h2>
       <p class="actions">
-        <a href="https://www.github.com">Github</a>
+        <a href="https://github.com/guozhq/simple-ui">Github</a>
         <router-link to="/doc/intro"> 开始 </router-link>
       </p>
     </div>
